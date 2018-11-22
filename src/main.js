cceed24c567d649d867d948dc369ebc71042e9e0
hongboo <791620392@qq.com>
2018-11-22 09:57:11 +0800

    添加h函数的描述

--- src/main.js
+++ src/main.js
@@ -13,3 +13,10 @@ new Vue({
   store,
   render: h => h(App) //vue2.0的新写法 h函数的实参是createElement
 }).$mount("#app");
+
+
+//render: h => h(App) 的完整写法：
+//将 h 作为 createElement 的别名是 Vue 生态系统中的一个通用惯例
+// function render(createElement) {
+//   return createElement(App)
+// }
